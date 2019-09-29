4dbeb1174cb276d5586594c1929fcadde8980870
eternal20 <54803853+eternal20@users.noreply.github.com>
2019-09-29 18:03:58 +0700

    Add files via upload

---
+++ pepperint/script.js
@@ -0,0 +1,32 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+	if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+		document.getElementById("header").className = "header-scroll";
+	}
+	else{
+		document.getElementById("header").className = "header";
+	}
+}
+
+const sidebar = document.getElementById("showSidebar");
+
+
+
+function showSidebar() {
+	document.getElementById("header").className = "header-click";
+	document.getElementById("whitebar1").className = "whitebar1x";
+	document.getElementById("whitebar2").className = "whitebar2x";
+	document.getElementById("whitebar3").className = "whitebar3x";
+	document.getElementById("navbar-menu").className = "navbar-menu-show cf";
+	document.getElementById("showSidebar").setAttribute("onclick", "closeSidebar()");
+}
+
+function closeSidebar(){
+	document.getElementById("header").className = "header";
+	document.getElementById("whitebar1").className = "whitebar1";
+	document.getElementById("whitebar2").className = "whitebar2";
+	document.getElementById("whitebar3").className = "whitebar3";
+	document.getElementById("navbar-menu").className = "navbar-menu";
+	document.getElementById("showSidebar").setAttribute("onclick", "showSidebar()");
+}
